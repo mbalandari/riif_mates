37c0ed0cfa5dd5983c185c0c79c208e031970029
codingMaz <m.balandari.toroghi@gmail.com>
2025-07-25 14:54:43 +0330

    add about page and info json and practice ch2

--- home/views.py
+++ home/views.py
@@ -1,6 +1,16 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 def credits(request):
     content = "Lala\nMaz"
     return HttpResponse(content, content_type="text/plain")
+
+
+def about(request):
+    content = "<html><body><h1>About Page</h1></body></html>"
+    return HttpResponse(content, content_type="text/html")
+
+
+def version_info(request):
+    data = {"site_version": "1.0.0"}
+    return JsonResponse(data, content_type="application/json")
